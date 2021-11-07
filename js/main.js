c204d9e0e5be2b0e72e21d7101c70fbe98793afa
IvanZaborskih <ivan.31.03@mail.ru>
2021-11-07 16:45:33 +0300

    upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        var blockId = $(this).data("scroll");
+        blockOffset = $(blockId).offset().top;
+
+        console.log(blockOffset);
+
+        $("html, body").animate({ scrollTop: blockOffset }, 500);
+    });
+
+});
+
+
+
+
